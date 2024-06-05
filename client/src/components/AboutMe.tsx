d1b643ed3a4d0ee17327bd486cabd756ddf113f6
Charlie Frey <charliesagefrey95@gmail.com>
2024-06-05 14:19:44 -0400

    refactored to use a node backend instead of python

---
+++ client/src/components/AboutMe.tsx
@@ -0,0 +1,55 @@
+import { Box } from "@mui/material";
+import { Suspense } from "react";
+import WhiteLink from "../widgets/WhiteLink";
+// import headshot from '../images/headshot.jpeg'
+
+const headshot = "https://i.imgur.com/NHf8ysU.jpeg";
+
+const AboutMe = () => {
+  return (
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          marginBottom: "6vw",
+        }}
+      >
+        <Box
+          sx={{
+            marginRight: "4vw",
+          }}
+        >
+          <Suspense>
+            <img
+              src={headshot}
+              style={{
+                height: "30vw",
+                width: "30vw",
+              }}
+            />
+          </Suspense>
+        </Box>
+        <Box>
+          Right now, I'm based in Grove City, PA. I'm hoping to move back to
+          Colorado Springs by the end of Summer 2024.
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          fontSize: "calc(20px + 1vw)",
+          marginBottom: "20px",
+          lineHeight: 1.6,
+        }}
+      >
+        After getting my Bachelor's degree in Computer Engineering from the
+        University of Dayton, I spent several years building software solutions
+        in the public and private sector.
+      </Box>
+      <Box sx={{ fontStyle: "italic" }}>
+        Take a look at my <WhiteLink href="/portfolio">portfolio</WhiteLink>.
+      </Box>
+    </Box>
+  );
+};
+
+export default AboutMe;
